42581a801fd522c34178b568fd8e7b2ad5e997fc
Aliaksandra <ryzhikai@rambler.ru>
2021-10-08 21:54:40 +0300

    6 attempt

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -3,8 +3,8 @@ package config;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class UserConfig {
-    public static final String USER_NAME = "Sasha";
-    public static final String USER_LOGIN = "tests@list.ru";
+    public static final String USER_NAME = "AliaksandraRyzhyk";
+    public static final String USER_LOGIN = "DemoTest@list.ru";
     public static final String USER_PASSWORD = "Password!1";
     public static final String USER_CONFIRM_PASSWORD = "Password!1";
 
